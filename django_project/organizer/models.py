52987eab948e94c4544ba8ae23fdb12e2cad7a5a
Bitcamp <hello@bitca.mp>
2015-06-20 20:42:31 -0400

    capitaliZation

--- django_project/organizer/models.py
+++ django_project/organizer/models.py
@@ -7,4 +7,4 @@ class Attendee(models.Model):
     last_name = models.CharField(max_length = 100)
     email = models.CharField(max_length = 100)
     school = models.CharField(max_length = 100)
-    image = models.Charfield(max_length = 100)
+    image = models.CharField(max_length = 100)
